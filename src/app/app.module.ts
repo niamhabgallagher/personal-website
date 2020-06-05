6053f4c3e675048c3dd7c990673769c5ce411bbf
Niamh Gallagher <niamhgallagher@niamhs-mbp.home>
2020-06-05 12:45:13 +0100

    Updating the home page, all the icons work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,12 +16,13 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    InAppBrowser
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from 'src/app/services/data/data.service';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +9,32 @@ import { DataService } from 'src/app/services/data/data.service';
 export class HomePage implements OnInit {
 
   constructor(
+    private inAppBrowser: InAppBrowser
   ) {}
 
   ngOnInit() {
+  }
+
+  openLinkedIn() {
+    const url = 'https://www.linkedin.com/in/niamhabgallagher/';
+    const linkedin = this.inAppBrowser.create(url, '_system');
+
+    linkedin.show();
+  }
+  // mailto:someone@example.com?Subject=Hello%20again
+
+  openGithub() {
+    const url = 'https://github.com/niamhabgallagher';
+    const github = this.inAppBrowser.create(url, '_system');
+
+    github.show();
+  }
+
+  sendEmail() {
+    const url = 'mailto:niamhgallagher1@yahoo.co.uk?Subject=From%20Niamh%20Gallagher\'s%20Website';
+    const email = this.inAppBrowser.create(url, '_system');
+
+    email.show();
+    console.log('email');
   }
 }
