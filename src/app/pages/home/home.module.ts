ca9786308da09a20e4e9fa1e36a5d219ad32812d
Niamh Gallagher <niamhgallagher@niamhs-imac.home>
2020-06-08 17:30:55 +0100

    Removing unused parts of code

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-// import { PagesModule } from '../pages.module';
 
 
 @NgModule({
@@ -14,7 +13,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     FormsModule,
     IonicModule,
     HomePageRoutingModule,
-    // PagesModule
   ],
   declarations: [HomePage]
 })
